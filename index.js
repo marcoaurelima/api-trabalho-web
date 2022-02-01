89364de7aae3c94b40ae0ee54cf31ee59375a9ff
Marco Aurelio Lima <marco_aurelio029@hotmail.com>
2022-02-01 16:09:25 -0300

    mudança na mensagem de teste

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  return res.status(200).json({message: 'tudo funcionando! Marco'})
+  return res.status(200).json({message: 'tudo funcionando! 1610'})
 })
 
 app.listen(process.env.PORT || 3000)
