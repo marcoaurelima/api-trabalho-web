09b787d425211e403fe1105b4e8003d8fd27d9c0
Marco Aurelio Lima <marco_aurelio029@hotmail.com>
2022-02-01 16:32:27 -0300

    mudança na mensagem

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.use(express.json())
 const getListaAll = require('./routes/lista/getListaAll')
 
 app.get('/', (req, res) => {
-  return res.status(200).json({message: 'tudo funcionando! 1610'})
+  return res.status(200).json({message: 'tudo funcionando! 1632'})
 })
 
 app.get('/lista', getListaAll)
