8a91bb99256444d13f3a5dfa2b6883d2c2e1db5a
Marco Aurelio Lima <marco_aurelio029@hotmail.com>
2022-02-01 18:22:36 -0300

    novas mudanças

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ console.log(process.env.DATABASE_PASSWORD)
 const getListaAll = require('./routes/lista/getListaAll')
 
 app.get('/', (req, res) => {
-  return res.status(200).json({message: 'tudo funcionando! 1734'})
+  return res.status(200).json({message: 'tudo funcionando! 1822'})
 })
 
 app.get('/list', getListaAll)
