2d49c5caf8dac3b4a3e8bfe3400822ad5eed955d
Marco Aurelio Lima <marco_aurelio029@hotmail.com>
2022-02-02 17:29:28 -0300

    inserção do middleware cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 require('dotenv-safe').config()
 
+var cors = require('cors')
 const express = require('express')
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 const login = require('./routes/users/login')
 const logon = require('./routes/users/logon')
@@ -12,7 +14,7 @@ const getListAll = require('./routes/list-item/getList')
 app.post('/login', login)
 app.post('/logon', logon)
 app.get('/', (req, res) => {
-  return res.status(200).json({cadeira: 'Programação web: equipe - Marco, Vinicus e Nathan'})
+  return res.status(200).json({equipe: 'Programação web: equipe - Marco, Vinicus e Nathan - cors'})
 })
 
 app.get('/list', getListAll)
