aae25b70275ef4985a85165118a849a738125cc6
Marco Aurelio <marco_aurelio029@hotmail.com>
2022-02-03 15:10:31 -0300

    Alteração do nome da rora getList para getListItems

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 require('dotenv-safe').config()
 
 const express = require('express')
-const app = express()
 
+const app = express()
+app.use(express.json())
 var cors = require('cors');
 app.use(cors());
-app.use(express.json())
-
 app.use(function(req, res, next) {
      res.header("Access-Control-Allow-Origin", "*");
      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -15,15 +14,14 @@ app.use(function(req, res, next) {
 
 const login = require('./routes/users/login')
 const logon = require('./routes/users/logon')
-const getListAll = require('./routes/list-item/getList')
+const getListAll = require('./routes/list-item/getListItems')
 
-
-app.post('/login', login)
-app.post('/logon', logon)
 app.get('/', (req, res) => {
   return res.status(200).json({equipe: 'Programação web: equipe - Marco, Vinicus e Nathan - cors(1)'})
 })
 
+app.post('/login', login)
+app.post('/logon', logon)
 app.get('/list', getListAll)
 
 
