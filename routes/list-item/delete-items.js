32db6065c3706b5accb7b1da4f479fd9f97fcfaf
Marco Aurelio <marco_aurelio029@hotmail.com>
2022-02-04 09:43:23 -0300

    teste

--- routes/list-item/delete-items.js
+++ routes/list-item/delete-items.js
@@ -3,15 +3,19 @@ const ListItem = require('../../database/models/list_item')
 const deleteItems = async (req, res) => {
   const {id_items} = req.body
 
+  return res.json({servidor: id_items})
+  /*
+
   try{
     id_items.map(async (id)=>{
       await ListItem.destroy({ where: {id} })
     })
     res.json({status: "ok", message: "Ítens deletados com sucesso!"})
-    
+
   }catch(e){
     res.json({status: "error", message: "Não foi possível deletar os ítens."})
   }
+  */
 }
 
 module.exports = deleteItems
