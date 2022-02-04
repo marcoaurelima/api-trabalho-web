b1adbd694e9bce2d694cf73f6c49e0785083917a
Marco Aurelio <marco_aurelio029@hotmail.com>
2022-02-04 12:44:56 -0300

    nova msg teste

--- routes/info.js
+++ routes/info.js
@@ -2,7 +2,7 @@ const info = (req, res) => {
   res.json({
     projeto: "Trabalho final de Web",
     equipe: "Marco, Vinícius e Natan",
-    ultima_alteraçao_API: "04/02/2022 11:36"
+    ultima_alteraçao_API: "04/02/2022 12:44"
   })
 }
 
